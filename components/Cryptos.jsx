53e5266f353c05c8be93cfb7b1324b9d0b544698
hlpires <higorlpires@gmail.com>
2022-09-22 12:03:04 -0300

    commit

--- components/Cryptos.jsx
+++ components/Cryptos.jsx
@@ -12,43 +12,53 @@ const Cryptos = () => {
     const [bitcoinNome, setBitCoinNome] = useState()
     const [bitcoinPrice, setBitCoinPrice] = useState()
     const [label, setlabel] = useState([])
+    const [bitcoinChange, setBitCoinChange] = useState()
+    const [color,setColor] = useState()
+    const myStyle = {
+        color:'red',
+        fontSize:'20px',
+        marginLeft:'0%',
 
-useEffect(() => {
-
-    const options = {
-        method: 'GET',
-        url: 'https://coinranking1.p.rapidapi.com/coins',
-        params: {
-            referenceCurrencyUuid: 'yhjMzLPhuIDl',
-            timePeriod: '24h',
-            'tiers[0]': '1',
-            orderBy: 'marketCap',
-            orderDirection: 'desc',
-            limit: '50',
-             offset: '0'
-         },
-        headers: {
-            'X-RapidAPI-Key': 'b810cbd18bmsh9be1172c850c4d8p100877jsn2839fe298a4c',
-            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-        }
-    };
-    axios.request(options).then(function (response) {
-        setDataCrypto(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
-
-}, []);
-
-useEffect(() => {
-    if (dataCrypto) {
-        setBitCoinNome(dataCrypto.data.coins[0].name)
-        setBitCoinPrice(Math.trunc(Number(dataCrypto.data.coins[0].price)))
-        setlabel(dataCrypto.data.coins[0].sparkline.map(item => Math.trunc(Number(item))))
+    
     }
-}, [dataCrypto]);
 
-console.log(label)
+    useEffect(() => {
+
+        const options = {
+            method: 'GET',
+            url: 'https://coinranking1.p.rapidapi.com/coins',
+            params: {
+                referenceCurrencyUuid: 'yhjMzLPhuIDl',
+                timePeriod: '24h',
+                'tiers[0]': '1',
+                orderBy: 'marketCap',
+                orderDirection: 'desc',
+                limit: '50',
+                offset: '0'
+            },
+            headers: {
+                'X-RapidAPI-Key': 'b810cbd18bmsh9be1172c850c4d8p100877jsn2839fe298a4c',
+                'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+            }
+        };
+        axios.request(options).then(function (response) {
+            setDataCrypto(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+    }, []);
+
+    useEffect(() => {
+        if (dataCrypto) {
+            setBitCoinNome(dataCrypto.data.coins[0].name)
+            setBitCoinPrice(Math.trunc(Number(dataCrypto.data.coins[0].price)))
+            setBitCoinChange(Number(dataCrypto.data.coins[0].change))
+            setlabel(dataCrypto.data.coins[0].sparkline.map(item => Math.trunc(Number(item))))
+        }
+    }, [dataCrypto]);
+
+    console.log(...label.slice(0,14))
 
     const data = {
 
@@ -61,16 +71,18 @@ console.log(label)
                     }
                 },
                 label: '',
-                data: [label[0], label[5], label[10], label[15], label[20], label[5]],
+                data: [...label.slice(0,7)],
                 elements: {
                     line: {
                         tension: 0,
                         fill: 'start',
                         borderWidth: 2,
-                        borderColor: 'rgba(47,297,68,1)',
+                        color: 'black',
+                        borderColor: 'rgba(47,197,268,1)',
                         backgroundColor: 'rgba(47,297,68,0.3)'
                     },
                     point: {
+
                         radius: 0,
                         girRadius: 0,
                     }
@@ -90,32 +102,44 @@ console.log(label)
 
 
 
-
-    return (
-        <div className='crypto'>
-            <div className='position'>
-                <div className='bubbleBox'>
-                    <div className='bubble'></div>
-                    <div className='bubble2'></div>
-                </div>
-                <div className='cardCryptos'>
-                    <div className='bitcoinCard'>
-                        <div className='headerTittle'><p className='tittle'>{bitcoinNome + ' Preço: ' + bitcoinPrice}</p></div>
-                        <div className='headerGraph'>
-                            <Line
-                                data={data}
-                                width={400}
-                                height={270}
-                            />
+    if (typeof dataCrypto !== 'undefined')
+        return (
+            <div className='crypto'>
+                <div className='position'>
+                    <div className='cryptoBox'>
+                        <div className='bubbleBox'>
+                            <div className='bubble'></div>
+                            <div className='bubble2'></div>
                         </div>
+                        <div className='cardCryptos'>
+                            <div className='bitcoinCard'>
+                                <div className='headerTittle'>
+                                    <div className = 'bitcoinImage'><Image loader={() => dataCrypto.data.coins[0].iconUrl} width={100} height={100} src={dataCrypto.data.coins[0].iconUrl}></Image></div>
+                                    <p className='tittle'>{bitcoinNome + '\t'}
+                                        <span className='spanTittle'>{bitcoinPrice + '$ '}</span>
+                                        <span   style = {myStyle} className='spanTittle'>{bitcoinChange+ '%' }</span>
+                                        <span className ='hora'>{ ' 24hr'}</span>
+                                    </p>
+                                </div>
+
+                                <div className='headerGraph'>
+                                    <Line
+                                        data={data}
+                                        width={400}
+                                        height={220}
+                                    />
+                                </div>
+                            </div>
+                            <div className='bitcoinCardImage'>
+                                <div className = 'bitcoinArt'><Image height={520} width={480} src={imageCrypto}></Image></div>
+                            </div>
+                        </div>
+                        <div className='allCrypto'></div>
                     </div>
-                    <div className='bitcoinCardImage'>
-                        <Image src={imageCrypto}></Image>
-                    </div>
+
                 </div>
             </div>
-        </div>
-    )
+        )
 }
 
 export default Cryptos
--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
 
+    return config;
+  }
+};
 module.exports = nextConfig
